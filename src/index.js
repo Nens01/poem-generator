ab232b758a2c1c679aa9e634eb191a7571784feb
Nens01 <neneeoguchi@icloud.com>
2025-05-16 00:24:25 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
 function displayPoem(event) {
   new Typewriter("#poem", {
-    strings: "Nwanyi bu ife, a na-azọ ya ka ego.",
+    strings: [event.data.poem],
     autoStart: true,
-    delay: 0.4,
+    delay: 400,
     cursor: "",
   });
 }
 function generatePoem(event) {
   event.preventDefault();
+
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "96t02003264bec3oa3b933aaca0f777f";
   let prompt = `User instructions: write a poem in igbo language about ${instructionsInput.value}`;
   let context =
     "You are a romantic igbo man, and love to write poems in igbo language, your mission is to write a 4 line poem in igbo language in basic HTML, be sure to follow the user instructions";
-  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key={apiKey}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   console.log("Generating poem...");
   console.log(`prompt: ${prompt}`);
